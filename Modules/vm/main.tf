e54a077697431c61dfd0d04f426635e568f2bbbd
AlexGonzalez2201 <88511001+AlexGonzalez2201@users.noreply.github.com>
2022-08-29 14:04:27 -0500

    Update main.tf

--- Modules/vm/main.tf
+++ Modules/vm/main.tf
@@ -1,9 +1,9 @@
 resource "google_service_account" "default" {
-  account_id   = "service_account_id"
+  account_id   = "service-account-id"
   display_name = "Service Account"
 }
 
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "test" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
