fb76efbb4a189370c570ce2257409692b6153e93
AlexGonzalez2201 <88511001+AlexGonzalez2201@users.noreply.github.com>
2022-08-29 13:42:18 -0500

    Update main.tf

--- Modules/vm/main.tf
+++ Modules/vm/main.tf
@@ -37,7 +37,7 @@ resource "google_compute_instance" "default" {
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
+    email  = google-service-account.default.email
     scopes = ["cloud-platform"]
   }
 }
